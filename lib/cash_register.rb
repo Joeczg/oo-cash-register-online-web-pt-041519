585f5dd1913e529092800dc2acb1fd077ea21fa1
zhenggao cai <caizhengao1992@gmail.com>
2019-05-22 18:35:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,23 @@
 class CashRegister 
-  attr_accessor :total , :discount
+  attr_accessor :total , :discount , :title
   def initialize(discount)
     @total = 0 
     @discount = discount
+    @title = []
   end
   
   def add_item(title,price,quality = 1)
-    count = 0     if quality > 1 
+    count = 0    
+    if quality > 1 
     while count < quality
-
+    @title << title
+    count += 1 
+     end
       @total += price * quality
-    
+    else 
+      @total += price
+      @title << title
+    end
+  end
+  
     
